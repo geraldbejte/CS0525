47a1c5966006d0217ba5c81019051ae1c4d6a64c
geraldbejte <geradbejte@gmail.com>
2025-12-04 12:10:06 -0500

    Pratica S2/L3

---
+++ UNIT_1/S2/L3/es_01.py
@@ -0,0 +1,10 @@
+testo1 = "Indovina il nome della band"
+print(testo1); 
+
+Città = input("Inserisci il nome della tua città: ")
+Animale_Domestico = input("Inserisci il nome del tuo Animale Domenisto: ")
+
+print(f"il nome della band è : {Città + Animale_Domestico}" )
+
+testo2 = "Complimenti hai vinto un biglietto per il concerto!"
+print(testo2) 
